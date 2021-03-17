16ee5545b200bf197bb0f2e4ad6d3f5d2fd49bf8
daddyUrsa <0057747@gmail.com>
2021-03-17 21:06:16 +0300

    Add tableView.

--- FileManager/ViewController.swift
+++ FileManager/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped )
+        let queue = DispatchQueue.global(qos: .utility)
+//        tableView.dataSource = self
+//        tableView.delegate = self
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+//        tableView.register(RepoTableViewCell.self, forCellReuseIdentifier: cellID)
+
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .brown
     }
 
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        return UITableViewCell()
+    }
+    
+    
+}
